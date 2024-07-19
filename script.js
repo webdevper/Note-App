32f98685fd7f3ed3be3646229ba40337b5e094f3
Mohan Jaiswal <webdevper.info@gmail.com>
2024-07-20 01:42:22 +0530

    modify

---
+++ script.js
@@ -0,0 +1,14 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box")
+
+
+createBtn.addEventListener("click", () =>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+inputBox.setAttribute("contenteditable", "true");    
+img.src ="Images/delete.png";
+notesContainer.appendChild(inputBox).appendChild(img);
+
+})
