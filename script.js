1c8ed6ce630d665c2225206789264e3b1f95948b
Mohan Jaiswal <webdevper.info@gmail.com>
2024-07-22 00:30:43 +0530

    final add js

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
-let notes = document.querySelectorAll(".input-box")
+let notes = document.querySelectorAll(".input-box");
 
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes") || "";
+}
 
-createBtn.addEventListener("click", () =>{
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
-inputBox.setAttribute("contenteditable", "true");    
-img.src ="Image/delete.png";
-notesContainer.appendChild(inputBox).appendChild(img);
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "Image/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
 
-})
+    updateStorage();  // Save to local storage after creating a new note
+});
 
-notesContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "IMG"){
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
+        updateStorage();  // Save to local storage after deleting a note
+    }
+    else if (e.target.tagName === "P" || e.target.tagName === "p") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();  // Save to local storage when content is edited
+            }
+        });
+    }
+});
+
+
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
